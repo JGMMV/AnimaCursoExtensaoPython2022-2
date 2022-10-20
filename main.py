db387777e2b3835cad35e990f7832792d943adf1
João Gabriel Menezes Moradillo Vieira <116229158+JGMMV@users.noreply.github.com>
2022-10-19 22:03:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@
 #print() = Comando de saida
 print('Alo mundo!')
 
+# Quando quiser guardar um String! (frase)
+nome = 'JGMV'
+#Quando quiser guardar um número inteiro
+idade = 32
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#Quando quiser exibir a frase ''minha idade é '' completando com o conteúdo da variável idade
+print('Minha idadde é '+str(idade))
+
 
 
 '''
